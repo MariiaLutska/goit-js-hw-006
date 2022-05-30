3ead1527a70da803074fb8817491a8664d995ce2
MariiaLutska <myklush@ukr.net>
2022-05-31 00:48:13 +0300

    8,9

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -9,3 +9,26 @@
 // Для доступу до елементів форми використовуй властивість elements.
 // Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
 
+const submitForm = document.querySelector('.login-form');
+const submitButton = document.querySelector('button');
+const emailField = document.querySelector('input[type="email"]').value;
+const passwordField = document.querySelector('input[type="password"]').value;
+
+
+let user = {
+    email: '',
+    password: '',
+};
+submitForm.addEventListener('submit', () => {
+    if (emailField || passwordField === '') {
+        alert('All field must be filled!!!');
+    } else {
+        user.email = emailField;
+        user.password = passwordField;
+    }
+    return user;
+
+    document.querySelector('.login-form').reset();
+});
+
+console.log(user);
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,32 @@
+const bodyEl = document.querySelector('body');
+const buttonRef = document.querySelector('.change-color');
+const textColorEl = document.querySelector('.color');
+
+buttonRef.addEventListener("click", onChangeColor);
+
+function onChangeColor() {
+  const hexColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = hexColor;
+  textColorEl.textContent = hexColor;
+};
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку
+//  на button.change - color і виводить значення кольору в span.color.
